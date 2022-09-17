8d373870c2843ab11a7aba963967fd9e111874d5
Rajan Lama <rajan.lama786@gmail.com>
2022-09-17 23:55:15 +0545

    update

--- index.php
+++ index.php
@@ -6,3 +6,4 @@ echo "This is just for yolo badge with developer4";
 echo "This is just for yolo badge with developer5";
 echo "This is just for yolo badge with developer6";
 echo "This is just for yolo badge with developer7";
+echo "This is just for yolo badge with developer8";
