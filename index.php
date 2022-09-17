bbe9003a30b2776108ed66acc56ca3e721b8c8ea
Rajan Lama <rajan.lama786@gmail.com>
2022-09-17 23:38:45 +0545

    yolo badge

--- index.php
+++ index.php
@@ -3,3 +3,4 @@ echo "This is just for yolo badge with developer";
 echo "This is just for yolo badge with developer2";
 echo "This is just for yolo badge with developer3";
 echo "This is just for yolo badge with developer4";
+echo "This is just for yolo badge with developer5";
